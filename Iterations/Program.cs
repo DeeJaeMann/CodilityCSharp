3cc277907da5bb2840f8c20471ed11157fe5a35c
Dee J. Mann <dee.j.mann@hotmail.com>
2026-09-18 13:16:29 -0400

    Added solution

--- Iterations/Program.cs
+++ Iterations/Program.cs
@@ -1,46 +1,99 @@
 ﻿// For loops
-for (int fIndex = 0; fIndex < 5; fIndex++ )
-{
-    Console.WriteLine($"For Index: {fIndex}");
-}
+// for (int fIndex = 0; fIndex < 5; fIndex++ )
+// {
+//     Console.WriteLine($"For Index: {fIndex}");
+// }
 // While loops
-int wIndex = 0;
-while (wIndex < 5)
-{
-    Console.WriteLine($"While Index: {wIndex}");
-    wIndex++;
-}
+// int wIndex = 0;
+// while (wIndex < 5)
+// {
+//     Console.WriteLine($"While Index: {wIndex}");
+//     wIndex++;
+// }
 // Loop over collections
-string[] daysArray = {"Monday",  "Tuesday", "Wednesday", "Thursday", "Friday"};
-
-Console.WriteLine($"Days Length: {daysArray.Length}");
-
-foreach (string day in daysArray)
-{
-    Console.WriteLine($"Array Day: {day}");
-}
+// string[] daysArray = {"Monday",  "Tuesday", "Wednesday", "Thursday", "Friday"};
+//
+// Console.WriteLine($"Days Length: {daysArray.Length}");
+//
+// foreach (string day in daysArray)
+// {
+//     Console.WriteLine($"Array Day: {day}");
+// }
 // HashSet
-HashSet<string> daysSet = new();
-daysSet.Add("Monday");
-daysSet.Add("Tuesday");
-daysSet.Add("Wednesday");
-daysSet.Add("Thursday");
-daysSet.Add("Friday");
-
-foreach (string day in daysSet)
-{
-    Console.WriteLine($"HashSet Day: {day}");
-}
+// HashSet<string> daysSet = new();
+// daysSet.Add("Monday");
+// daysSet.Add("Tuesday");
+// daysSet.Add("Wednesday");
+// daysSet.Add("Thursday");
+// daysSet.Add("Friday");
+//
+// foreach (string day in daysSet)
+// {
+//     Console.WriteLine($"HashSet Day: {day}");
+// }
 
 // Dictionary
-Dictionary<string, string> DictDays = new();
-DictDays.Add("mon", "Monday");
-DictDays.Add("tue", "Tuesday");
-DictDays.Add("wed", "Wednesday");
-DictDays.Add("thur", "Thursday");
-DictDays.Add("fri", "Friday");
+// Dictionary<string, string> DictDays = new();
+// DictDays.Add("mon", "Monday");
+// DictDays.Add("tue", "Tuesday");
+// DictDays.Add("wed", "Wednesday");
+// DictDays.Add("thur", "Thursday");
+// DictDays.Add("fri", "Friday");
+//
+// foreach ((string key, string value) in DictDays)
+// {
+//     Console.WriteLine($"{key} stands for {value}");
+// }
 
-foreach ((string key, string value) in DictDays)
+// BinaryGap
+/* A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at
+ both ends in the binary representation of N. For example, number 9 has binary representation 1001 and contains a binary 
+ gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and 
+ one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 
+ has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary 
+ gaps.   
+ Write a function:  
+   class Solution { public int solution(int N); }
+   
+ that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N 
+ doesn't contain a binary gap.  
+   For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its 
+   longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation 
+   '100000' and thus no binary gaps.
+   
+ Write an efficient algorithm for the following assumptions:
+  - N is an integer within the range [1..2,147,483,647].
+ */
+
+Console.WriteLine($"Solution: {new Solution().solution(1041)}");
+Console.WriteLine($"Solution: {new Solution().solution(32)}");
+Console.WriteLine($"Solution: {new Solution().solution(9)}");
+Console.WriteLine($"Solution: {new Solution().solution(529)}");
+Console.WriteLine($"Solution: {new Solution().solution(20)}");
+Console.WriteLine($"Solution: {new Solution().solution(15)}");
+
+class Solution
 {
-    Console.WriteLine($"{key} stands for {value}");
+    public int solution(int N)
+    {
+        string binSignature = Convert.ToString(N, 2);
+        int maxGap = 0;
+        int tmpGap = 0;
+        Console.WriteLine(binSignature);
+        if (binSignature.Contains("0"))
+        {
+            foreach (char c in binSignature)
+            {
+                if (c == '1')
+                {
+                    if (tmpGap > maxGap) maxGap = tmpGap;
+                    tmpGap = 0;
+                }
+                if (c == '0') tmpGap++;
+            }  
+        }
+
+        return maxGap;
+    }
 }
+
